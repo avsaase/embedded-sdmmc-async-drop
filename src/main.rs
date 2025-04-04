9e224c5c6c7fe0bd88a46fbc707972612fd61f2d
Alexander van Saase <avsaase@gmail.com>
2025-04-04 07:55:52 +0200

    Blocking version works

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
+use core::{cell::RefCell, fmt::Write};
 
 use defmt::{error, info, warn};
-use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
+use embassy_embedded_hal::shared_bus::blocking::spi::SpiDevice;
 use embassy_executor::Spawner;
+use embassy_futures::yield_now;
 use embassy_rp::{
     gpio::{Level, Output},
     peripherals::{self},
     spi::{self, Config, Spi},
 };
-use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_time::{block_for, Delay, Duration, Timer};
-use embedded_hal_async::spi::SpiDevice as _;
+use embedded_hal::spi::SpiDevice as _;
 use embedded_sdmmc::{
-    asynchronous::{Mode, SdCard, VolumeIdx, VolumeManager},
+    blocking::{Mode, SdCard, VolumeIdx, VolumeManager},
     blocking::{TimeSource, Timestamp},
 };
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
-type SpiBus = Mutex<CriticalSectionRawMutex, Spi<'static, peripherals::SPI0, spi::Async>>;
+type SpiBus = embassy_sync::blocking_mutex::Mutex<
+    CriticalSectionRawMutex,
+    RefCell<Spi<'static, peripherals::SPI0, spi::Blocking>>,
+>;
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -30,11 +34,10 @@ async fn main(spawner: Spawner) {
 
     let mut spi_config = Config::default();
     spi_config.frequency = 400_000;
-    let spi: Spi<'_, embassy_rp::peripherals::SPI0, embassy_rp::spi::Async> = Spi::new(
-        p.SPI0, p.PIN_18, p.PIN_19, p.PIN_16, p.DMA_CH4, p.DMA_CH5, spi_config,
-    );
+    let spi: Spi<'_, embassy_rp::peripherals::SPI0, embassy_rp::spi::Blocking> =
+        Spi::new_blocking(p.SPI0, p.PIN_18, p.PIN_19, p.PIN_16, spi_config);
     static SPI_BUS: StaticCell<SpiBus> = StaticCell::new();
-    let spi_bus = SPI_BUS.init(Mutex::new(spi));
+    let spi_bus = SPI_BUS.init(embassy_sync::blocking_mutex::Mutex::new(RefCell::new(spi)));
     let cs_sd = Output::new(p.PIN_23, Level::High);
     let cs_other = Output::new(p.PIN_24, Level::High);
 
@@ -44,22 +47,23 @@ async fn main(spawner: Spawner) {
 
 #[embassy_executor::task]
 async fn sd_task(bus: &'static SpiBus, mut cs: Output<'static>) {
-    cs.set_high();
-    bus.lock().await.write(&[0xFF; 10]).await.unwrap();
-
-    bus.lock().await.set_frequency(25_000_000);
+    bus.lock(|b| {
+        let mut spi = b.borrow_mut();
+        cs.set_high();
+        spi.blocking_write(&[0xFF; 10]).unwrap();
+        spi.set_frequency(25_000_000)
+    });
 
     let spi_device = SpiDevice::new(bus, cs);
     let sd_card = SdCard::new(spi_device, Delay);
     let volume_manager = VolumeManager::new(sd_card, DummyTimeSource);
 
-    let volume0 = volume_manager.open_volume(VolumeIdx(0)).await.unwrap();
+    let volume0 = volume_manager.open_volume(VolumeIdx(0)).unwrap();
 
     let root_dir = volume0.open_root_dir().unwrap();
 
     let new_file = root_dir
         .open_file_in_dir("TEST.TXT", Mode::ReadWriteCreateOrTruncate)
-        .await
         .unwrap();
 
     let mut buf = heapless::String::<64>::new();
@@ -69,7 +73,7 @@ async fn sd_task(bus: &'static SpiBus, mut cs: Output<'static>) {
         write!(buf, "Line: {:05}\n", i).unwrap();
         'retry: loop {
             attempt += 1;
-            match new_file.write(buf.as_bytes()).await {
+            match new_file.write(buf.as_bytes()) {
                 Ok(_) => {
                     // info!("Successfully wrote to file at attempt {}", attempt);
                     break 'retry;
@@ -84,10 +88,11 @@ async fn sd_task(bus: &'static SpiBus, mut cs: Output<'static>) {
             }
         }
         buf.clear();
+        yield_now().await;
     }
-    new_file.close().await.unwrap();
+    new_file.close().unwrap();
     root_dir.close().unwrap();
-    volume0.close().await.unwrap();
+    volume0.close().unwrap();
     info!("SD task done");
 }
 
@@ -98,10 +103,10 @@ async fn other_task(bus: &'static SpiBus, cs: Output<'static>) {
         Timer::after(Duration::from_millis(100)).await;
         block_for(Duration::from_millis(10));
         // Write some dummy bytes
-        spi_device.write(&[0x55; 10]).await.unwrap();
-        spi_device.write(&[0x55; 10]).await.unwrap();
-        spi_device.write(&[0x55; 10]).await.unwrap();
-        spi_device.write(&[0x55; 10]).await.unwrap();
+        spi_device.write(&[0x55; 10]).unwrap();
+        spi_device.write(&[0x55; 10]).unwrap();
+        spi_device.write(&[0x55; 10]).unwrap();
+        spi_device.write(&[0x55; 10]).unwrap();
     }
 }
 
